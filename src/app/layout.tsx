77940064a61f1f80d5573d037b81fc6889cf80d6
hunter <linhunter231@gmail.com>
2025-10-30 18:13:32 +0800

    change ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "莆仙话拼音查询",
   description: "查询汉字对应的莆仙话拼音",
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
@@ -17,18 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN">
-       <body className={inter.className}>
-         {children}
-         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z7CMHP0JWN" />
-         <Script id="google-analytics" strategy="afterInteractive">
-           {`
-             window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-             gtag('js', new Date());
-             gtag('config', 'G-Z7CMHP0JWN');
-           `}
-         </Script>
-       </body>
+        <body className={inter.className}>
+          {children}
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z7CMHP0JWN" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Z7CMHP0JWN');
+            `}
+          </Script>
+        </body>
     </html>
   );
 }
