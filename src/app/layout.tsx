c45822401a04e7e7663035ab45bbfd24c0bedeb4
hunter <linhunter231@gmail.com>
2025-10-30 17:53:54 +0800

    add google analtics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN">
-      <body className={inter.className}>{children}</body>
+       <body className={inter.className}>
+         {children}
+         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z7CMHP0JWN" />
+         <Script id="google-analytics" strategy="afterInteractive">
+           {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', 'G-Z7CMHP0JWN');
+           `}
+         </Script>
+       </body>
     </html>
   );
 }
